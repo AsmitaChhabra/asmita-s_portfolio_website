f6249c7d76e04d74fab799b5c8ce62f6784bdbfb
Asmita Chhabra <asmita.chhabra@flame.edu.in>
2025-10-05 16:02:58 +0530

    FINAL NGINX FIX: Switched startup.sh to use NGINX_CONF_FILE environment variable for writeable path.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cp /home/site/wwwroot/default /etc/nginx/sites-available/default
-nginx -g 'daemon off;'
+
+# 1. Copy the custom config file (default) to the writeable directory
+cp /home/site/wwwroot/default /home/site/default
+
+# 2. Tell NGINX to use the custom config file from the writeable path
+export NGINX_CONF_FILE=/home/site/default
+
+# 3. Start NGINX using the standard command
+# This command is often what App Service uses internally to start the web server.
+/usr/sbin/nginx -c $NGINX_CONF_FILE -g 'daemon off;'
